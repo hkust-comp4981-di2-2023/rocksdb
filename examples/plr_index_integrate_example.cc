48155c35bf341ecfff7d19aa1dd34e9d15c9eeb2
codermomo <alexmo201898@gmail.com>
2024-02-17 23:36:08 +0800

    updated example

--- examples/plr_index_integrate_example.cc
+++ examples/plr_index_integrate_example.cc
@@ -7,6 +7,7 @@
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
 #include "table/block_based/index_builder.h"
+#include "table/block_based/learned_index/plr/plr_block_iter.h"
 #include "table/format.h"
 
 using namespace ROCKSDB_NAMESPACE;
@@ -22,6 +23,90 @@ BlockHandle MakeBlockHandle(const int& offset_and_size) {
 }
 
 int main() {
-  // TODO(fyp): start from here (mainly for debugging) before writing test cases
+  // Index block content building using PLRIndexBuilder
+  printf("===== Index block content building using PLRIndexBuilder =====\n");
+  
+  std::vector<std::string> tmp = {"yamada", "anna", "totemo", "kawaii", 
+    "ichikawa", "kyotaro", "mo", "kakkoi",
+    "kanojo", "dekite", "hoshii"};
+  std::sort(tmp.begin(), tmp.end());
+  std::vector<Slice> keys;
+  std::transform(tmp.begin(), tmp.end(), std::back_inserter(keys), MakeSlice);
+
+  std::vector<uint64_t> offset_and_size = {1, 12, 56, 78,
+    91, 126, 954, 1045,
+    5506, 20687, 50489};
+  std::vector<BlockHandle> bh;
+  std::transform(offset_and_size.begin(), offset_and_size.end(), 
+                  std::back_inserter(bh), MakeBlockHandle);
+  bh[0].set_offset(1111);
+
+  size_t num_data_block = keys.size();
+  assert(bh.size() == keys.size());
+
+  double gamma = 0.3;
+  PLRIndexBuilder* builder = new PLRIndexBuilder(gamma);
+
+
+  builder->OnKeyAdded(keys[0]);
+  std::vector<Slice>::const_iterator it = keys.begin();
+  it++;
+  std::vector<BlockHandle>::const_iterator bhit = bh.begin();
+  for (; it != keys.end(); it++, bhit++) {
+    builder->AddIndexEntry(nullptr, &(*it), *bhit);
+  }
+  builder->AddIndexEntry(nullptr, nullptr, *bhit);
+
+  std::string result;
+  IndexBuilder::IndexBlocks ib;
+  ib.index_block_contents = MakeSlice(result);
+
+  builder->Finish(&ib, *bhit);
+
+  printf("Encoded string size: %d\n", ib.index_block_contents.size());
+  for (size_t i = 0; i < ib.index_block_contents.size(); ++i) {
+    printf("%lu;", ib.index_block_contents.data()[i]);
+  }
+  printf("\n");
+
+  delete builder;
+
+  // Index block content reading using PLRBlockHelper
+  printf("===== Index block content reading using PLRBlockHelper =====\n");
+
+  PLRBlockHelper reader(num_data_block, ib.index_block_contents);
+
+  std::vector<std::string> sorted_data_block_keys = {"anna", "dekite", "hoshii",
+    "ichikawa", "kakkoi", "kanojo", "kawaii", 
+    "kyotaro", "mo", "totemo", "yamada"};
+  std::map<std::string, BlockHandle> data_blocks;
+  for (size_t i = 0; i < num_data_block; ++i) {
+    data_blocks[sorted_data_block_keys[i]] = bh[i];
+  }
+  printf("Trained with the following data blocks (Gamma = %.2f):\n", gamma);
+  for (const auto& pair : data_blocks) {
+    printf("%s -> %s\n", pair->first, pair->second.TosString());
+  }
+
+  uint64_t begin_num, end_num;
+  BlockHandle begin_bh, end_bh;
+  std::vector<std::string> targets = {
+    "tenki", "ga", "ii", "kara", "sanpo", "shimasho", 
+    "i", "go", "to", "school", "by", "bus",
+    "never", "gonna", "give", "you", "up",
+    "never", "gonna", "let", "you", "down"};
+
+  printf("\n");
+  printf("Testing with the following target keys (Gamma = %.2f):\n", gamma);
+  for (std::vector<std::string>::const_iterator it = targets.begin(); 
+        it != targets.end(); it++) {
+    reader.PredictBlockRange(*it, begin_num, end_num);
+    reader.GetBlockHandle(begin_num, begin_bh);
+    reader.GetBlockHandle(end_num, end_bh);
+    printf("Target [%s] -> Output [Begin (block#: %lu; handle: %s)"
+            "End (block#: %lu; handle: %s)]\n",
+            *it, begin_num, begin_bh.ToString(), end_num, end_bh.ToString());
+  }
+
   return 0;
 }
