e501c23228958379176a71215d3e454b71c79c6a
Oscar-0713 <nctse@connect.ust.hk>
2024-03-18 13:13:35 +0800

    [Update] Change Default configuration

--- tools/db_crashtest.py
+++ tools/db_crashtest.py
@@ -63,7 +63,7 @@ default_params = {
     "max_key": 100000000,
     "max_write_buffer_number": 3,
     "mmap_read": lambda: random.randint(0, 1),
-    "nooverwritepercent": 1,
+    "nooverwritepercent": 0,
     "open_files": lambda : random.choice([-1, 500000]),
     "partition_filters": lambda: random.randint(0, 1),
     "pause_background_one_in": 1000000,
