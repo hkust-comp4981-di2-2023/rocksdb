c9e152ac8e3171e8112126be7b99781d400aea28
codermomo <alexmo201898@gmail.com>
2024-03-31 13:49:58 +0800

    updated test case

--- table/block_based/block_test.cc
+++ table/block_based/block_test.cc
@@ -879,29 +879,25 @@ TEST_P(PLRIndexBlockTest, PLRIndexValueEncodingTest) {
         // last_keys[seek_result_index].c_str());
       
       // check if the extracted block_handle matches the one in block_handles
-      if (seek_result_index == expected_index) {
-        break;
-      } else {
-        Slice seek_result_first_key(first_keys[seek_result_index]);
-        Slice seek_result_last_key(last_keys[seek_result_index]);
-        iter->UpdateBinarySeekRange(query_key, seek_result_first_key, 
-                                    seek_result_last_key);
-        if (iter->IsLastBinarySeek()) {
-          iter->SwitchToLinearSeekMode();
-          // Special handling for cases where multiple internal keys with the
-          // same user key but different seq_no exist.
-          while (iter->Valid() && icomp.Compare(query_key, seek_result_last_key) > 0) {
-            iter->Next();
-            if (iter->Valid()) {
-              v = iter->value();
-              seek_result_index = reverse_index[v.handle.offset()];
-              seek_result_last_key = Slice(last_keys[seek_result_index]);
-            }
+      Slice seek_result_first_key(first_keys[seek_result_index]);
+      Slice seek_result_last_key(last_keys[seek_result_index]);
+      iter->UpdateBinarySeekRange(query_key, seek_result_first_key, 
+                                  seek_result_last_key);
+      if (iter->IsLastBinarySeek()) {
+        iter->SwitchToLinearSeekMode();
+        // Special handling for cases where multiple internal keys with the
+        // same user key but different seq_no exist.
+        while (iter->Valid() && icomp.Compare(query_key, seek_result_last_key) > 0) {
+          iter->Next();
+          if (iter->Valid()) {
+            v = iter->value();
+            seek_result_index = reverse_index[v.handle.offset()];
+            seek_result_last_key = Slice(last_keys[seek_result_index]);
           }
-          break;
         }
-        iter->Next();
+        break;
       }
+      iter->Next();
     }
     
     // EXPECT_EQ(separators[expected_index], iter->key().ToString());
@@ -930,29 +926,25 @@ TEST_P(PLRIndexBlockTest, PLRIndexValueEncodingTest) {
       int seek_result_index = reverse_index[v.handle.offset()];
       
       // check if the extracted block_handle matches the one in block_handles
-      if (seek_result_index == expected_index) {
-        break;
-      } else {
-        Slice seek_result_first_key(first_keys[seek_result_index]);
-        Slice seek_result_last_key(last_keys[seek_result_index]);
-        iter->UpdateBinarySeekRange(query_key, seek_result_first_key, 
-                                    seek_result_last_key);
-        if (iter->IsLastBinarySeek()) {
-          iter->SwitchToLinearSeekMode();
-          // Special handling for cases where multiple internal keys with the
-          // same user key but different seq_no exist.
-          while (iter->Valid() && icomp.Compare(query_key, seek_result_last_key) > 0) {
-            iter->Next();
-            if (iter->Valid()) {
-              v = iter->value();
-              seek_result_index = reverse_index[v.handle.offset()];
-              seek_result_last_key = Slice(last_keys[seek_result_index]);
-            }
+      Slice seek_result_first_key(first_keys[seek_result_index]);
+      Slice seek_result_last_key(last_keys[seek_result_index]);
+      iter->UpdateBinarySeekRange(query_key, seek_result_first_key, 
+                                  seek_result_last_key);
+      if (iter->IsLastBinarySeek()) {
+        iter->SwitchToLinearSeekMode();
+        // Special handling for cases where multiple internal keys with the
+        // same user key but different seq_no exist.
+        while (iter->Valid() && icomp.Compare(query_key, seek_result_last_key) > 0) {
+          iter->Next();
+          if (iter->Valid()) {
+            v = iter->value();
+            seek_result_index = reverse_index[v.handle.offset()];
+            seek_result_last_key = Slice(last_keys[seek_result_index]);
           }
-          break;
         }
-        iter->Next();
+        break;
       }
+      iter->Next();
     }
     
     // EXPECT_EQ(separators[expected_index], iter->key().ToString());
@@ -1010,6 +1002,8 @@ TEST_P(PLRIndexBlockTest, PLRIndexValueEncodingTest) {
     if (iter->Valid()) {
       EXPECT_EQ(block_handles[expected_index+1].offset(), v.handle.offset());
       EXPECT_EQ(block_handles[expected_index+1].size(), v.handle.size());
+    } else {
+      EXPECT_EQ(expected_index+1, num_records);
     }
   }
   delete iter;
