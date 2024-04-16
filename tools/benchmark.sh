8c038231a985fa61e442fff1c05706fbbdc7f09d
Cyrologistic <iclemo0.0@gmail.com>
2024-04-16 13:36:01 +0800

    added overwrite

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1084,6 +1084,45 @@ function run_fyp_exponential_e {
   summarize_result $output_dir/${out_name} fyp_exponential_e readrandomwriterandom
 }
 
+function run_fyp_exponential_overwrite {
+  echo "Loading $num_keys random keys"
+  cmd="./db_bench --benchmarks=fillrandom,stats \
+       --use_existing_db=0 \
+       --sync=0 \
+       --report_file="exponential_a.csv" \
+       --report_interval_seconds=30 \
+       --duration=1200 \
+       --key_dist_a=1 \
+       --key_dist_b=2.718 \
+       $params_bulkload \
+       --threads=16 \
+       --memtablerep=vector \
+       --allow_concurrent_memtable_write=false \
+       --disable_wal=1 \
+       --seed=4981 \
+       2>&1 | tee -a $output_dir/benchmark_fyp_exponential_a.log"
+  echo $cmd | tee $output_dir/benchmark_fyp_exponential_a.log
+  eval $cmd
+
+  echo "Do $num_keys random overwrite"
+  out_name="benchmark_overwrite.log"
+  cmd="./db_bench --benchmarks=overwrite \
+       --use_existing_db=1 \
+       --sync=$syncval \
+       --report_file="exponential_overwrite.csv" \
+       --report_interval_seconds=30 \
+       --duration=1200 \
+       --key_dist_a=1 \
+       --key_dist_b=2.718 \
+       $params_w \
+       --threads=16 \
+       --merge_operator=\"put\" \
+       --seed=4981 \
+       2>&1 | tee -a $output_dir/${out_name}"
+  echo $cmd | tee $output_dir/${out_name}
+  eval $cmd
+}
+
 function now() {
   echo `date +"%s"`
 }
@@ -1143,6 +1182,8 @@ for job in ${jobs[@]}; do
     run_fyp_exponential_d
   elif [ $job = fyp_exponential_e ]; then
     run_fyp_exponential_e
+  elif [ $job = fyp_exponential_overwrite ]; then
+    run_fyp_exponential_overwrite 
   elif [ $job = fillseq_disable_wal ]; then
     run_fillseq 1
   elif [ $job = fillseq_enable_wal ]; then
